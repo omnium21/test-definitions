12996af7ff61b72c28d127a3ba59ae98947980b9
Ryan Harkin <ryan.harkin@linaro.org>
2020-10-06 17:30:15 +0100

    add client-finished
    This should probably be part of a generic client script

---
+++ automated/linux/ethernet/client-finished.sh
@@ -0,0 +1,50 @@
+#!/bin/sh -ex
+
+# shellcheck disable=SC1091
+. ../../lib/sh-test-lib
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+LOGFILE="${OUTPUT}/iperf.txt"
+# If SERVER is blank, we are the server, otherwise
+# If we are the client, we set SERVER to the ipaddr of the server
+SERVER=""
+# Time in seconds to transmit for
+TIME="10"
+# Number of parallel client streams to run
+THREADS="1"
+# Specify iperf3 version for CentOS.
+VERSION="3.1.4"
+# By default, the client sends to the server,
+# Setting REVERSE="-R" means the server sends to the client
+REVERSE=""
+# CPU affinity is blank by default, meaning no affinity.
+# CPU numbers are zero based, eg AFFINITY="-A 0" for the first CPU
+AFFINITY=""
+ETH="eth0"
+
+usage() {
+    echo "Usage: $0 [-c server] [-e server ethernet device] [-t time] [-p number] [-v version] [-A cpu affinity] [-R] [-s true|false]" 1>&2
+    exit 1
+}
+
+while getopts "A:c:e:t:p:v:s:Rh" o; do
+  case "$o" in
+    A) AFFINITY="-A ${OPTARG}" ;;
+    c) SERVER="${OPTARG}" ;;
+    e) ETH="${OPTARG}" ;;
+    t) TIME="${OPTARG}" ;;
+    p) THREADS="${OPTARG}" ;;
+    R) REVERSE="-R" ;;
+    v) VERSION="${OPTARG}" ;;
+    s) SKIP_INSTALL="${OPTARG}" ;;
+    h|*) usage ;;
+  esac
+done
+
+create_out_dir "${OUTPUT}"
+cd "${OUTPUT}"
+
+cmd="lava-send"
+if which "${cmd}"; then
+    ${cmd} client-request request="finished"
+fi

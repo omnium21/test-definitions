4cd4a770ec465dafd3442e29d04dd805b205c042
Ryan Harkin <ryan.harkin@linaro.org>
2020-10-08 13:49:05 +0100

    check ping result from server vs expected result

--- automated/linux/ethernet/client-ping.sh
+++ automated/linux/ethernet/client-ping.sh
@@ -5,44 +5,60 @@
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 ETH="eth0"
+EXPECTED_RESULT="pass"
 
 create_out_dir "${OUTPUT}"
 cd "${OUTPUT}"
 
-while getopts "A:c:e:t:p:v:s:Rh" o; do
+while getopts "A:c:e:t:p:v:s:r:Rh" o; do
   case "$o" in
+    A) AFFINITY="-A ${OPTARG}" ;;
+    c) SERVER="${OPTARG}" ;;
     e) ETH="${OPTARG}" ;;
+    t) TIME="${OPTARG}" ;;
+    p) THREADS="${OPTARG}" ;;
+    r) EXPECTED_RESULT="${OPTARG}" ;;
+    R) REVERSE="-R" ;;
+    v) VERSION="${OPTARG}" ;;
+    s) SKIP_INSTALL="${OPTARG}" ;;
     h|*) usage ;;
   esac
 done
 
+command_exists(){
+	local cmd
+
+	cmd="$1"
+
+	if ! which "${cmd}"; then
+		echo "ERROR: ${cmd} not available"
+		exit 1
+	fi
+}
+command_exists "lava-echo-ipv4"
+command_exists "lava-send"
+command_exists "lava-wait"
+command_exists "ping"
+
 # Run local iperf3 server as a daemon when testing localhost.
-cmd="lava-echo-ipv4"
-if which "${cmd}"; then
-    ipaddr=$(${cmd} "${ETH}" | tr -d '\0')
-    if [ -z "${ipaddr}" ]; then
-        lava-test-raise "${ETH} not found"
-    fi
-	cmd="lava-send"
-	if which "${cmd}"; then
-		${cmd} client-request request="ping" ipaddr="${ipaddr}"
-	fi
-
-	# TODO - wait for a response
-	cmd="lava-wait"
-	if which "${cmd}"; then
-		${cmd} client-ping-done
-	fi
-
-	# TODO - report pass/fail depending on whether we expected ping to succeed or not
-	result=$(grep "result" /tmp/lava_multi_node_cache.txt | awk -F"=" '{print $NF}')
-	echo "The daemon says that pinging the client returned ${result}"
-	if [ "${result}" = 0 ]; then
-		echo "ping: pass"
-	else
-		echo "ping: fail"
-	fi
-else
-    echo "WARNING: command ${cmd} not found. We are not running in the LAVA environment."
+ipaddr=$(lava-echo-ipv4 "${ETH}" | tr -d '\0')
+if [ -z "${ipaddr}" ]; then
+	lava-test-raise "${ETH} not found"
 fi
+lava-send client-request request="ping" ipaddr="${ipaddr}"
 
+# wait for a response
+lava-wait client-ping-done
+
+# report pass/fail depending on whether we expected ping to succeed or not
+result=$(grep "result" /tmp/lava_multi_node_cache.txt | awk -F"=" '{print $NF}')
+echo "The daemon says that pinging the client returned ${result}"
+echo "We are expecting ping to ${EXPECTED_RESULT}"
+if [ "${result}" = "0" ] && [ "${EXPECTED_RESULT}" == "pass" ];
+	actual_result="pass"
+elif [ "${result}" != "0" ] && [ "${EXPECTED_RESULT}" == "fail" ];
+	actual_result="pass"
+else
+	actual_result="fail"
+fi
+echo "client-ping-request ${actual_result}" | tee -a "${RESULT_FILE}"

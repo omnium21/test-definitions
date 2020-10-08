1cc3981d84da0bc5e395f8701c380b2ae0569b40
Ryan Harkin <ryan.harkin@linaro.org>
2020-10-08 14:33:30 +0100

    handle ping failure when running 'sh -ex'

--- automated/linux/ethernet/daemon.sh
+++ automated/linux/ethernet/daemon.sh
@@ -108,8 +108,8 @@ while [ true ]; do
 		"ping")
 			echo "Client has asked us to ping address ${ipaddr}"
 			ipaddr=$(grep "ipaddr" /tmp/lava_multi_node_cache.txt | awk -F"=" '{print $NF}')
-			ping -c 5 "${ipaddr}"
-			result="$?"
+			result=0
+			ping -c 5 "${ipaddr}" || result="$?"
 			cmd="lava-send"
 			if which "${cmd}"; then
 				${cmd} client-ping-done result="${result}"

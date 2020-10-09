882bb6541a645b03188cbc5d0f3c4307857e31ff
Ryan Harkin <ryan.harkin@linaro.org>
2020-10-09 20:25:02 +0100

    fix

--- automated/linux/ethernet/client-ping.sh
+++ automated/linux/ethernet/client-ping.sh
@@ -75,7 +75,7 @@ if [ "${ipaddr}" != "" ]; then
 	# wait for a new response
 	while [ true ]; do
 		lava-wait client-ping-done
-		rx_datestr=$(grep "datestr" /tmp/lava_multi_node_cache.txt | tail -1 awk -F"=" '{print $NF}')
+		rx_datestr=$(grep "datestr" /tmp/lava_multi_node_cache.txt | tail -1 | awk -F"=" '{print $NF}')
 
 		if [ "${tx_datestr}" != "${rx_datestr}" ]; then
 			echo "WARNING: skipping old message ${rx_datestr} when waiting for ${tx_datastr}"
